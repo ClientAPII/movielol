9bbfa6b37460392fb8d0d2eb3864c34c4e474873
ClientAPI <simso0508@gmail.com>
2025-02-26 01:06:04 +0100

    131231

--- movie.lua
+++ movie.lua
@@ -11,7 +11,7 @@ end
 -- 3) Den Bildschirm konfigurieren
 monitor.setBackgroundColor(colors.black)
 monitor.setTextColor(colors.white)
-monitor.setTextScale(4)
+monitor.setTextScale(2)
 monitor.clear()
 
 -- 4) Terminal-Ausgaben auf den Monitor umleiten
